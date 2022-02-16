6b85002bf941aa9da051af322d8f41cd9fe6eac5
Amir Mehr <amirhoseinsharifimehr@gmail.com>
2022-02-16 11:20:20 +0330

    Update MehrGateServiceProvider.php

--- src/MehrGateServiceProvider.php
+++ src/MehrGateServiceProvider.php
@@ -9,7 +9,7 @@ class MehrGateServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('mehrGate', function () {
-            return new mehrGate;
+            return new MehrGate;
         });
 
         $this->mergeConfigFrom( __DIR__ . '/Config/mehr-gate-config.php', 'mehr-gate');
